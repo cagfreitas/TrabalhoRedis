2a396c95b79c3079b9a4e2f984eb967f1b577f9e
Camila Freitas <camila.freitas@dtidigital.com.br>
2023-09-10 10:51:20 -0300

    removed unused function

--- src/services/messageService.js
+++ src/services/messageService.js
@@ -15,33 +15,3 @@ exports.getMessage = async (receivedMessage) => {
     console.error("Erro ao recuperar mensagem:", error);
   }
 }
-
-exports.publishMessage = (channel, value) =>{
-  redisPubClient.publish(channel, value);
-}
-
-exports.subscribe = (channelSubscribed, callback) => {
-  redisClient.subscribe(channelSubscribed, (channel, message)=>{
-    console.log('Message arrived')
-    console.log('serv'+ message)
-    callback(message)
-  })
-}
-
-// async function cacheMessage(req, res) { 
- 
-//   try {
-//   const mensagemRecebida = req.body.mensagem;
-
-//     const resposta = await redisClient.get(mensagemRecebida);
-
-//     if (resposta) {
-//       res.status(200).send({ resposta });
-//     } else {
-//       res.status(200).send({ resposta: "Não entendemos a pergunta." });
-//     }
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).send("Ocorreu um erro ao processar a solicitação.");
-//   }
-// };
